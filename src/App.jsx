bec5ded38ee1cd99ce9667a339d5cd8475e79491
allanschneitz-hash <allan.schneitz@pro-vision.fi>
2026-03-24 11:51:14 +0200

    Create App.jsx for Pro-Vision Keikkasuunnittelu component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Pro-Vision Keikkasuunnittelu</h1>
+            {/* Add your components here */}
+        </div>
+    );
+}
+
+export default App;
